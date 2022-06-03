342d7c1fd355c6b2173aad71f0514be92a15edf1
mutsuki <mutsuki@momo86.net>
2022-06-03 14:34:29 +0900

    Add `launch_stridedBatch_kernel` to auto code gen script

--- src/instances/gencode.sh
+++ src/instances/gencode.sh
@@ -13,6 +13,18 @@ template void cumpsgemm::launch_kernel<DATA_T, cumpsgemm::SMEM_M,cumpsgemm::SMEM
 			DATA_T* const c_ptr, const std::size_t ldc,
 			cudaStream_t cuda_stream
 		);
+template void cumpsgemm::launch_stridedBatch_kernel<DATA_T, cumpsgemm::SMEM_M,cumpsgemm::SMEM_N,cumpsgemm::SMEM_K, cumpsgemm::FRAG_M,cumpsgemm::FRAG_N,cumpsgemm::FRAG_K, cumpsgemm::BLOCK_SIZE, cumpsgemm::%s,cumpsgemm::%s, %s, mtk::wmma::tcec::%s> (
+			const std::size_t m,
+			const std::size_t n,
+			const std::size_t k,
+			const DATA_T alpha,
+			const DATA_T* const a_ptr, const std::size_t lda, const std::size_t stridea,
+			const DATA_T* const b_ptr, const std::size_t ldb, const std::size_t strideb,
+			const DATA_T beta,
+			DATA_T* const c_ptr, const std::size_t ldc, const std::size_t stridec,
+			const std::size_t num_blocks_per_gemm,
+			cudaStream_t cuda_stream
+		);
 EOF
 )
 
@@ -24,7 +36,7 @@ for T in float cuComplex;do
 					TCT_name=$(echo ${TCT} | sed -e 's/:://g')
 					file_name="ins_${T}_${OPA}_${OPB}_${TCT_name}_${EC}.cu"
 					echo "#include \"../cumpsgemm_kernel.cuh\"" > $file_name
-					echo $(printf "$TEMPLATE" $T $OPA $OPB $TCT $EC) >> $file_name
+					echo $(printf "$TEMPLATE" $T $OPA $OPB $TCT $EC $OPA $OPB $TCT $EC) >> $file_name
 				done
 			done
 		done

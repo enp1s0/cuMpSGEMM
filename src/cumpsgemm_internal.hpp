ecebe9622e51c9e04ca1944d291e0ec30af94331
mutsuki <mutsuki@momo86.net>
2022-06-29 20:46:59 +0900

    Fix blocking size

--- src/cumpsgemm_internal.hpp
+++ src/cumpsgemm_internal.hpp
@@ -63,7 +63,7 @@ constexpr unsigned SMEM_N = 64;
 constexpr unsigned SMEM_K = 32;
 constexpr unsigned FRAG_M = 32;
 constexpr unsigned FRAG_N = 32;
-constexpr unsigned FRAG_K = 32;
+constexpr unsigned FRAG_K = 16;
 constexpr unsigned BLOCK_SIZE = 128;
 } // namespace cumpsgemm
 #endif

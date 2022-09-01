9fe817f1f49f7d3c053b42f1cb09a651912e8da6
mutsuki <mutsuki@momo86.net>
2022-09-02 00:09:11 +0900

    Fix the layout of tcec::fragment

--- src/device_tcec_wrapper.hpp
+++ src/device_tcec_wrapper.hpp
@@ -9,14 +9,37 @@ namespace device {
 // fragment
 template <class T, class Use, unsigned M, unsigned N, unsigned K, class Layout, class TC_T, class EC>
 struct tc_fragment {
-	using frag_t = mtk::wmma::tcec::fragment<Use, M, N, K, TC_T, typename cumpsgemm::device::layout_conv<Layout>::type, typename mtk::wmma::tcec::default_policy<TC_T, EC>::type>;
+	using frag_t = mtk::wmma::tcec::fragment<Use, M, N, K, TC_T, void, typename mtk::wmma::tcec::default_policy<TC_T, EC, mtk::wmma::tcec::op_mma>::type>;
 	frag_t frag;
 };
 
-template <class Use, unsigned M, unsigned N, unsigned K, class Layout, class TC_T, class EC>
-struct tc_fragment<cuComplex, Use, M, N, K, Layout, TC_T, EC> {
-	using frag_t = mtk::wmma::tcec::fragment_complex<Use, M, N, K, TC_T, typename cumpsgemm::device::layout_conv<Layout>::type, typename mtk::wmma::tcec::default_policy<TC_T, EC>::type>;
+template <unsigned M, unsigned N, unsigned K, class Layout, class TC_T, class EC>
+struct tc_fragment<float, nvcuda::wmma::matrix_a, M, N, K, Layout, TC_T, EC> {
+	using frag_t = mtk::wmma::tcec::fragment<nvcuda::wmma::matrix_a, M, N, K, TC_T, nvcuda::wmma::row_major, typename mtk::wmma::tcec::default_policy<TC_T, EC, mtk::wmma::tcec::op_mma>::type>;
+	frag_t frag;
+};
 
+template <unsigned M, unsigned N, unsigned K, class Layout, class TC_T, class EC>
+struct tc_fragment<float, nvcuda::wmma::matrix_b, M, N, K, Layout, TC_T, EC> {
+	using frag_t = mtk::wmma::tcec::fragment<nvcuda::wmma::matrix_b, M, N, K, TC_T, nvcuda::wmma::col_major, typename mtk::wmma::tcec::default_policy<TC_T, EC, mtk::wmma::tcec::op_mma>::type>;
+	frag_t frag;
+};
+
+template <unsigned M, unsigned N, unsigned K, class Layout, class TC_T, class EC>
+struct tc_fragment<cuComplex, nvcuda::wmma::accumulator, M, N, K, Layout, TC_T, EC> {
+	using frag_t = mtk::wmma::tcec::fragment_complex<nvcuda::wmma::accumulator, M, N, K, TC_T, void, typename mtk::wmma::tcec::default_policy<TC_T, EC, mtk::wmma::tcec::op_mma>::type>;
+	frag_t frag;
+};
+
+template <unsigned M, unsigned N, unsigned K, class Layout, class TC_T, class EC>
+struct tc_fragment<cuComplex, nvcuda::wmma::matrix_a, M, N, K, Layout, TC_T, EC> {
+	using frag_t = mtk::wmma::tcec::fragment_complex<nvcuda::wmma::matrix_a, M, N, K, TC_T, nvcuda::wmma::row_major, typename mtk::wmma::tcec::default_policy<TC_T, EC, mtk::wmma::tcec::op_mma>::type>;
+	frag_t frag;
+};
+
+template <unsigned M, unsigned N, unsigned K, class Layout, class TC_T, class EC>
+struct tc_fragment<cuComplex, nvcuda::wmma::matrix_b, M, N, K, Layout, TC_T, EC> {
+	using frag_t = mtk::wmma::tcec::fragment_complex<nvcuda::wmma::matrix_b, M, N, K, TC_T, nvcuda::wmma::col_major, typename mtk::wmma::tcec::default_policy<TC_T, EC, mtk::wmma::tcec::op_mma>::type>;
 	frag_t frag;
 };
 
@@ -30,12 +53,22 @@ __device__ void fill_zero (
 
 // fragment loader
 template <class MEM_Layout, class MEM_T, class Use, unsigned M, unsigned N, unsigned K, class TC_T, class Layout, class EC>
+struct load_matrix_core {
+	__device__ void operator()(
+			tc_fragment<MEM_T, Use, M, N, K, Layout, TC_T, EC>& frag,
+			const MEM_T* const ptr,
+			const uint64_t ldm
+			) {
+	}
+};
+
+template <class MEM_Layout, class MEM_T, class Use, unsigned M, unsigned N, unsigned K, class TC_T, class Layout, class EC>
 __device__ void load_matrix(
 		tc_fragment<MEM_T, Use, M, N, K, Layout, TC_T, EC>& frag,
 		const MEM_T* const ptr,
 		const uint64_t ldm
 		) {
-	mtk::wmma::tcec::load_matrix_sync<typename cumpsgemm::device::layout_conv<MEM_Layout>::type>(frag.frag, ptr, ldm);
+	load_matrix_core<MEM_Layout, MEM_T, Use, M, N, K, TC_T, Layout, EC>{}(frag, ptr, ldm);
 }
 
 // fragment storer

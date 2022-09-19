90fbc6ca3697cb874fe49279f30351bc6bacf4d0
mutsuki <mutsuki@momo86.net>
2022-09-19 21:32:44 +0900

    Fix data type in Python API test

--- python/test/main.py
+++ python/test/main.py
@@ -1,8 +1,8 @@
 import cumpsgemm_hijack_control as chc
 import cupy
 
-a = cupy.random.rand(1000, 1000)
-b = cupy.random.rand(1000, 1000)
+a = cupy.random.rand(1000, 1000).astype('f')
+b = cupy.random.rand(1000, 1000).astype('f')
 
 compute_mode_list = [
         chc.CUMPSGEMM_FP16TCEC,

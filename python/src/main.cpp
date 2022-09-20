60d0166ecce96c15f4d7a2a8f3f3c3a06a481631
mutsuki <mutsuki@momo86.net>
2022-09-20 21:20:06 +0900

    Add exp_stats Python API for hijacking

--- python/src/main.cpp
+++ python/src/main.cpp
@@ -9,11 +9,35 @@ void unset_compute_mode() {
 	cumpsgemm::hijack_control::unset_compute_mode();
 }
 
+pybind11::list get_last_exp_stats() {
+	const auto result_list = cumpsgemm::hijack_control::get_last_exp_stats();
+	pybind11::list res;
+	for (const auto& r : result_list) {
+		pybind11::dict d;
+		d["total"] = r.first;
+		d["lost"]  = r.second;
+
+		res.append(d);
+	}
+	return res;
+}
+
+void enable_exp_stats() {
+	cumpsgemm::hijack_control::enable_exp_stats();
+}
+
+void disable_exp_stats() {
+	cumpsgemm::hijack_control::disable_exp_stats();
+}
+
 PYBIND11_MODULE(cumpsgemm_hijack_control, m) {
 	m.doc() = "cuMpSGEMM hijack control API";
 
 	m.def("unset_compute_mode", &unset_compute_mode, "unset_compute_mode");
 	m.def("set_compute_mode"  , &set_compute_mode  , "set_compute_mode"  , pybind11::arg("compute_mode"));
+	m.def("get_last_exp_stats", &get_last_exp_stats, "get_last_exp_stats");
+	m.def("enable_exp_stats"  , &enable_exp_stats  , "enable_exp_stats");
+	m.def("disable_exp_stats" , &disable_exp_stats , "disable_exp_stats");
 
 	pybind11::enum_<cuMpSGEMM_compute_mode_t>(m, "compute_mode")
 		.value("CUMPSGEMM_CUBLAS"       , CUMPSGEMM_CUBLAS       )

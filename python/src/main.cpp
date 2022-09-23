7a01a4375708c38edc48c14c6c1597b5d51d985c
mutsuki <mutsuki@momo86.net>
2022-09-23 21:38:35 +0900

    Add `global_lost_rate_threshold`

--- python/src/main.cpp
+++ python/src/main.cpp
@@ -1,6 +1,8 @@
 #include <pybind11/pybind11.h>
 #include <cumpsgemm/hijack_control.hpp>
 
+double global_lost_rate_threshold = 0.1;
+
 void set_compute_mode(const cuMpSGEMM_compute_mode_t compute_mode) {
 	cumpsgemm::hijack_control::set_compute_mode(compute_mode);
 }
@@ -37,15 +39,40 @@ void set_exp_stats_params(
 	cumpsgemm::hijack_control::set_exp_stats_params(ignore_threshold, lost_threshold);
 }
 
+void set_global_lost_rate_threshold(const double a) {
+	global_lost_rate_threshold = a;
+}
+
+float get_global_lost_rate_threshold() {
+	return global_lost_rate_threshold;
+}
+
+double get_lost_rate() {
+	const auto l = cumpsgemm::hijack_control::get_last_exp_stats();
+	std::size_t lost_count = 0;
+	std::size_t total_count = 0;
+	for (const auto es : l) {
+		total_count += es.second;
+		lost_count += es.first;
+	}
+	if (total_count > 0) {
+		return static_cast<double>(lost_count) / total_count;
+	}
+	return 0.;
+}
+
 PYBIND11_MODULE(cumpsgemm_hijack_control, m) {
 	m.doc() = "cuMpSGEMM hijack control API";
 
-	m.def("unset_compute_mode"  , &unset_compute_mode  , "unset_compute_mode");
-	m.def("set_compute_mode"    , &set_compute_mode    , "set_compute_mode"  , pybind11::arg("compute_mode"));
-	m.def("get_last_exp_stats"  , &get_last_exp_stats  , "get_last_exp_stats");
-	m.def("enable_exp_stats"    , &enable_exp_stats    , "enable_exp_stats");
-	m.def("disable_exp_stats"   , &disable_exp_stats   , "disable_exp_stats");
-	m.def("set_exp_stats_params", &set_exp_stats_params, "set_exp_stats_params", pybind11::arg("ignore_threshold"), pybind11::arg("lost_threshold"));
+	m.def("unset_compute_mode"            , &unset_compute_mode            , "unset_compute_mode");
+	m.def("set_compute_mode"              , &set_compute_mode              , "set_compute_mode"  , pybind11::arg("compute_mode"));
+	m.def("get_last_exp_stats"            , &get_last_exp_stats            , "get_last_exp_stats");
+	m.def("enable_exp_stats"              , &enable_exp_stats              , "enable_exp_stats");
+	m.def("disable_exp_stats"             , &disable_exp_stats             , "disable_exp_stats");
+	m.def("set_exp_stats_params"          , &set_exp_stats_params          , "set_exp_stats_params", pybind11::arg("ignore_threshold"), pybind11::arg("lost_threshold"));
+	m.def("set_global_lost_rate_threshold", &set_global_lost_rate_threshold, "set_global_lost_rate_threshold", pybind11::arg("rate_threshold"));
+	m.def("get_global_lost_rate_threshold", &get_global_lost_rate_threshold, "get_global_lost_rate_threshold");
+	m.def("get_lost_rate"                 , &get_lost_rate                 , "get_lost_rate");
 
 	pybind11::enum_<cuMpSGEMM_compute_mode_t>(m, "compute_mode")
 		.value("CUMPSGEMM_CUBLAS"       , CUMPSGEMM_CUBLAS       )

38c549301e1a5f8d50e10b57be6bf20f807ec660
mutsuki <mutsuki@momo86.net>
2022-10-01 14:00:33 +0900

    Add `global_auto_kernel_selection_enabled`

--- python/src/main.cpp
+++ python/src/main.cpp
@@ -3,6 +3,7 @@
 #include <cumpsgemm/hijack_control.hpp>
 
 double global_lost_ratio_threshold = 0.1;
+int global_auto_kernel_selection_enabled = 0;
 
 namespace cumpsgemm {
 namespace hijack_control {
@@ -41,6 +42,18 @@ void disable_exp_stats() {
 	cumpsgemm::hijack_control::disable_exp_stats();
 }
 
+void enable_auto_kernel_selection() {
+	global_auto_kernel_selection_enabled = 1;
+}
+
+void disable_auto_kernel_selection() {
+	global_auto_kernel_selection_enabled = 0;
+}
+
+bool is_auto_kernel_selection_enabled() {
+	return global_auto_kernel_selection_enabled;
+}
+
 void set_exp_stats_params(
 		const float ignore_threshold,
 		const float lost_threshold
@@ -77,17 +90,20 @@ unsigned get_current_buffer_id() {
 PYBIND11_MODULE(cumpsgemm_hijack_control, m) {
 	m.doc() = "cuMpSGEMM hijack control API";
 
-	m.def("unset_compute_mode"             , &unset_compute_mode             , "unset_compute_mode");
-	m.def("set_compute_mode"               , &set_compute_mode               , "set_compute_mode"  , pybind11::arg("compute_mode"));
-	m.def("get_exp_stats"                  , &get_exp_stats                  , "get_exp_stats", pybind11::arg("buffer_id"));
-	m.def("get_current_buffer_id"          , &get_current_buffer_id          , "get_current_buffer_id");
-	m.def("enable_exp_stats"               , &enable_exp_stats               , "enable_exp_stats");
-	m.def("disable_exp_stats"              , &disable_exp_stats              , "disable_exp_stats");
-	m.def("set_exp_stats_params"           , &set_exp_stats_params           , "set_exp_stats_params", pybind11::arg("ignore_threshold"), pybind11::arg("lost_threshold"));
-	m.def("set_global_lost_ratio_threshold", &set_global_lost_ratio_threshold, "set_global_lost_ratio_threshold", pybind11::arg("ratio_threshold"));
-	m.def("get_global_lost_ratio_threshold", &get_global_lost_ratio_threshold, "get_global_lost_ratio_threshold");
-	m.def("get_lost_ratio"                 , &get_lost_ratio                 , "get_lost_ratio", pybind11::arg("buffer_id"));
-	m.def("is_exp_stats_enabled"           , &is_exp_stats_enabled           , "is_exp_stats_enabled");
+	m.def("unset_compute_mode"              , &unset_compute_mode              , "unset_compute_mode");
+	m.def("set_compute_mode"                , &set_compute_mode                , "set_compute_mode"  , pybind11::arg("compute_mode"));
+	m.def("get_exp_stats"                   , &get_exp_stats                   , "get_exp_stats", pybind11::arg("buffer_id"));
+	m.def("get_current_buffer_id"           , &get_current_buffer_id           , "get_current_buffer_id");
+	m.def("enable_exp_stats"                , &enable_exp_stats                , "enable_exp_stats");
+	m.def("disable_exp_stats"               , &disable_exp_stats               , "disable_exp_stats");
+	m.def("set_exp_stats_params"            , &set_exp_stats_params            , "set_exp_stats_params", pybind11::arg("ignore_threshold"), pybind11::arg("lost_threshold"));
+	m.def("set_global_lost_ratio_threshold" , &set_global_lost_ratio_threshold , "set_global_lost_ratio_threshold", pybind11::arg("ratio_threshold"));
+	m.def("get_global_lost_ratio_threshold" , &get_global_lost_ratio_threshold , "get_global_lost_ratio_threshold");
+	m.def("get_lost_ratio"                  , &get_lost_ratio                  , "get_lost_ratio", pybind11::arg("buffer_id"));
+	m.def("is_exp_stats_enabled"            , &is_exp_stats_enabled            , "is_exp_stats_enabled");
+	m.def("enable_auto_kernel_selection"    , &enable_auto_kernel_selection    , "enable_auto_kernel_selection");
+	m.def("disable_auto_kernel_selection"   , &disable_auto_kernel_selection   , "disable_auto_kernel_selection");
+	m.def("is_auto_kernel_selection_enabled", &is_auto_kernel_selection_enabled, "is_auto_kernel_selection_enabled");
 
 	pybind11::enum_<cuMpSGEMM_compute_mode_t>(m, "compute_mode")
 		.value("CUMPSGEMM_CUBLAS"       , CUMPSGEMM_CUBLAS       )

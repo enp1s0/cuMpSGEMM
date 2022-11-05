3c0b45e4f7c12544331591de5c04bfbe2d9d5af9
mutsuki <mutsuki@momo86.net>
2022-11-05 20:28:12 +0900

    Add CUMPSGEMM_FP16TCEC_SCALING

--- include/cumpsgemm/detail/common.h
+++ include/cumpsgemm/detail/common.h
@@ -4,15 +4,17 @@ struct cuMpSGEMM_handle;
 typedef cuMpSGEMM_handle* cuMpSGEMM_handle_t;
 
 enum cuMpSGEMM_compute_mode_t {
-	CUMPSGEMM_CUBLAS          = 0,
-	CUMPSGEMM_FP16TCEC        = 1,
-	CUMPSGEMM_TF32TCEC        = 2,
-	CUMPSGEMM_FP16TC          = 3,
-	CUMPSGEMM_TF32TC          = 4,
-	CUMPSGEMM_CUBLAS_SIMT     = 5,
-	CUMPSGEMM_CUBLAS_FP16TC   = 6,
-	CUMPSGEMM_CUBLAS_TF32TC   = 7,
-	CUMPSGEMM_DRY_RUN         = 8,
-	CUMPSGEMM_AUTO            = 9,
+	CUMPSGEMM_CUBLAS           = 0,
+	CUMPSGEMM_FP16TCEC         = 1,
+	CUMPSGEMM_TF32TCEC         = 2,
+	CUMPSGEMM_FP16TC           = 3,
+	CUMPSGEMM_TF32TC           = 4,
+	CUMPSGEMM_CUBLAS_SIMT      = 5,
+	CUMPSGEMM_CUBLAS_FP16TC    = 6,
+	CUMPSGEMM_CUBLAS_TF32TC    = 7,
+	CUMPSGEMM_DRY_RUN          = 8,
+	CUMPSGEMM_AUTO             = 9,
+	CUMPSGEMM_UNDEFINED        = 10,
+	CUMPSGEMM_FP16TCEC_SCALING = 11,
 };
 #endif

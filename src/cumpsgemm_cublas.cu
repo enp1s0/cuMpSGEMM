01ada9ecb5fcbae4e7bfc8d434c8b6290dde2e0b
mutsuki <mutsuki@momo86.net>
2022-12-18 00:24:09 +0900

    Add loss rate in AUT mode log

--- src/cumpsgemm_cublas.cu
+++ src/cumpsgemm_cublas.cu
@@ -691,10 +691,14 @@ cublasStatus_t cuMpSGEMM_stridedBatched_hijack_core(
 					const auto gemm_mode = cumpsgemm::dynamic_launch::utils::get_gemm_flag(flag);
 					const auto scale_A = cumpsgemm::dynamic_launch::utils::get_scale_A_flag(flag);
 					const auto scale_B = cumpsgemm::dynamic_launch::utils::get_scale_B_flag(flag);
+					const auto loss_rate_A = cumpsgemm::get_exp_stats(cuMpSGEMM_get_internal_global_handle(), A_exp_stats_id);
+					const auto loss_rate_B = cumpsgemm::get_exp_stats(cuMpSGEMM_get_internal_global_handle(), B_exp_stats_id);
 					cuMpSGEMM_log(std::string("AUTO[ignore<") + std::to_string(cuMpSGEMM_get_internal_global_handle()->exp_stats_handle->ignore_threshold) + ", uf<"
 							+ std::to_string(cuMpSGEMM_get_internal_global_handle()->exp_stats_handle->underflow_threshold) + ", tolerance="
 							+ std::to_string(cuMpSGEMM_get_internal_global_handle()->exp_stats_handle->underflow_tolerance_rate)
-							+ "]: GEMM_MODE=" + cuMpSGEMM_get_compute_mode_string((cuMpSGEMM_compute_mode_t)gemm_mode) + ", scale_A=" + std::to_string(scale_A) + ", scale_B=" + std::to_string(scale_B));
+							+ "]: GEMM_MODE=" + cuMpSGEMM_get_compute_mode_string((cuMpSGEMM_compute_mode_t)gemm_mode)
+							+ ", loss_A=" + std::to_string(loss_rate_A.first) + "/" + std::to_string(loss_rate_A.second) + ", scale_A=" + std::to_string(scale_A)
+							+ ", loss_B=" + std::to_string(loss_rate_B.first) + "/" + std::to_string(loss_rate_B.second) + ", scale_B=" + std::to_string(scale_B));
 					});
 
 			// Scaling
